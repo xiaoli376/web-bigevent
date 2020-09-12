5c8bd017aa6c2fb2b70ef10b5a2cc4069fb4ae5f
栗亚星 <1179693181@qq.con>
2020-09-12 10:53:37 +0800

    1111

---
+++ assets/js/login.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('#link-reg').on('click',function(){
+       $('.login-box').hide()
+       $('.reg-box').show() 
+    })
+    $('#link-login').on('click',function(){
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+})
